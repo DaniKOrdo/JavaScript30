69c7c83b9a2ec2b501d606a829c6a19dae90280c
DaniKOrdo <hola@danik.dev>
2023-02-22 15:25:41 +0100

    add config

--- 08 - Fun with HTML5 Canvas/js/app.js
+++ 08 - Fun with HTML5 Canvas/js/app.js
@@ -1,11 +1,17 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
+const board = document.querySelector('.board');
+const color = document.querySelector('#color');
+const range = document.querySelector('#range');
+const clear = document.querySelector('#clear');
+
+
+canvas.width = board.offsetWidth;
+canvas.height = board.offsetHeight;
+ctx.strokeStyle = color.value;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+ctx.lineWidth = range.value;
 // ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
@@ -14,10 +20,15 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
+function config() {
+    console.log(color.value);
+    ctx.strokeStyle = color.value;
+    ctx.lineWidth = range.value;
+}
+
 function draw(e) {
     if (!isDrawing) return; // stop the fn from running when they are not moused down
-    console.log(e);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    // ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
@@ -30,15 +41,17 @@ function draw(e) {
     if (hue >= 360) {
         hue = 0;
     }
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-        direction = !direction;
-    }
 
-    if(direction) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
-    }
+
+    // if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    //     direction = !direction;
+    // }
+
+    // if(direction) {
+    //     ctx.lineWidth++;
+    // } else {
+    //     ctx.lineWidth--;
+    // }
 
 }
 
@@ -51,3 +64,7 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+color.addEventListener('change', config);
+range.addEventListener('change', config);
+clear.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
